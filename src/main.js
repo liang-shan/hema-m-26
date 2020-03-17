d14ba79d8c6eef807012457de469b0b0209904fa
liang-shan <LXL20806151218@126.com>
2020-03-17 22:12:02 +0800

    rem适配

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入移动组件大亨vant
+import 'vant/lib/index.css' // 引入vant组件的样式
+
+import 'amfe-flexible'// 随着屏幕的变化字体跟着变化
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
