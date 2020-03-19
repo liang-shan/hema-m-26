8e3390740a26c64cb8fa49136e9017ea2c407bd1
liang-shan <LXL20806151218@126.com>
2020-03-19 22:12:36 +0800

    token失效下集

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@
 
 import axios from 'axios'
 import JSONBig from 'json-bigint' // 引入大数字插件
-import store from '@store' // 引入大数字插件
+import store from '@store'
 import router from './router'
 
 const instance = axios.create({ // 相当于new了一个新的实例
@@ -41,22 +41,50 @@ instance.interceptors.response.use(function (response) {
     //  如果失败 说明 response.data不存在  如果两层解不开 就返回一层
     return response.data
   }
-}, function (error) {
+}, async function (error) {
+  // error 里边有三个对象  config 错误的配置信息  request  错误的请求对象  response  错误的响应对象
   // 响应拦截器只要是401  就是token的问题  就要处理token
   if (error.response && error.response.status === 401) {
+    const path = {
+      path: '/login',
+      query: {
+        // 需要传递的query参数
+        redirectUrl: router.currentRoute.fullPath // 表示登录页需要跳转的地址
+      }
+    }
     if (store.state.user.refresh_token) {
       // 如果有就说明你有救命的稻草
+      // 这里用axios来换取新的token ,  因为显得token失效,用instense的话还会将失效的token注入一直循环下去
 
+      try {
+        const result = await axios({
+
+          // 此时需要穿完整的地址,因为基地址是给instense设置的这用不了
+          url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+          method: 'put',
+          headers: { authorization: `Bearer ${store.state.user.refresh_token}` }
+        })
+        // result.data.data.token // 这就换取了新的token
+        // 需要把新的token从新设置给  用到store的设施token的方法
+        store.commit('updateToken', {
+          user: {
+            token: result.data.data.token,
+            refresh_token: store.state.user.refresh_token
+          }
+        })
+
+        return instance(error.config)// 就相当于把之前401的错误又执行了一遍,换取了新的token
+      } catch (error) {
+        // 回到登入页面  删除token
+        store.commit('delUser') // 删除token
+        router.push(path)
+      }
     } else {
       // 没有直接跳到登入页面
       // 从A页面实效的返回到登入页面成功之后还想回到A页面
-      router.push({
-        path: '/login',
-        query: {
-          // 需要传递的query参数
-          redirectUrl: router.currentRoute.fullPath // 表示登录页需要跳转的地址
-        }
-      })
+      store.commit('delUser') // 删除token
+
+      router.push(path)
     }
   }
 })
