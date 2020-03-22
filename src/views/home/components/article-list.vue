25172698ad86d5b9c38e0e0ce1445d72318fe87c
liang-shan <LXL20806151218@126.com>
2020-03-22 23:03:00 +0800

    一张图三张图无图基本布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,14 +1,59 @@
 <template>
   <div class="scroll-wrapper">
-    <van-pull-refresh v-model="downLoading" @refresh="onRefresh"                         :success-text="refreshSuccessText">
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
       <van-list v-model="upLoading" :finished="finished" @load="onLoad">
         <van-cell-group>
           <van-cell
             v-for="item in articles"
             :key="item"
-            :title="`每股又荣炖了`+item"
-            :value="`行情不好`+item"
-          ></van-cell>
+
+          >
+            <!-- 三张图 -->
+            <div class="article_item">
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+
+              <!-- 一张图 -->
+
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+              <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+
+              <!-- 无图 -->
+
+              <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
         </van-cell-group>
       </van-list>
     </van-pull-refresh>
@@ -42,7 +87,10 @@ export default {
       // 触发下拉刷新
       console.log('下拉刷新')
       setTimeout(() => {
-        const arr = Array.from(Array(10), (value, index) => ('追加' + (index + 1)))
+        const arr = Array.from(
+          Array(10),
+          (value, index) => '追加' + (index + 1)
+        )
         this.articles.unshift(...arr) // 将数据添加到队首
         this.downLoading = false // 关掉下拉状态
         this.refreshSuccessText = `更新了${arr.length}条数据`
@@ -52,5 +100,45 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
